1f547eca9e80a6f69682b9505136aa7faa4f66e5
catsflex <ilovedonuts2812@gmail.com>
2026-03-15 00:15:45 +0300

    remove possible name collisions

--- src/main/java/me/catsflex/test/config/YACLIntegration.java
+++ src/main/java/me/catsflex/test/config/YACLIntegration.java
@@ -13,23 +13,23 @@ public class YACLIntegration {
 		var config = TestConfig.getInstance();
 		
 		return YetAnotherConfigLib.createBuilder()
-			.title(Component.translatable("config.title"))
+			.title(Component.translatable("config.test.title"))
 			
 			// 'General' category
-			.category(ConfigCategory.createBuilder().name(Component.translatable("config.category.general"))
+			.category(ConfigCategory.createBuilder().name(Component.translatable("config.test.category.general"))
 				
 				// 'Enabled' option
 				.option(Option.<Boolean>createBuilder()
-					.name(Component.translatable("config.option.enabled.name"))
-					.description(OptionDescription.of(Component.translatable("config.option.enabled.desc")))
+					.name(Component.translatable("config.test.option.enabled.name"))
+					.description(OptionDescription.of(Component.translatable("config.test.option.enabled.desc")))
 					.binding(TestConfig.DEF_IS_ENABLED, () -> config.isEnabled, v -> config.isEnabled = v)
 					.controller(TickBoxControllerBuilder::create)
 					.build())
 				
 				// 'Debug mode' option
 				.option(Option.<Boolean>createBuilder()
-					.name(Component.translatable("config.option.debug.name"))
-					.description(OptionDescription.of(Component.translatable("config.option.debug.desc")))
+					.name(Component.translatable("config.test.option.debug.name"))
+					.description(OptionDescription.of(Component.translatable("config.test.option.debug.desc")))
 					.binding(TestConfig.DEF_IS_DEBUG_MODE, () -> config.isDebugMode, v -> config.isDebugMode = v)
 					.controller(TickBoxControllerBuilder::create)
 					.build())
